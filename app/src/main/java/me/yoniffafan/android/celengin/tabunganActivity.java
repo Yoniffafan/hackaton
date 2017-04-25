75507d368c8e8da39f4e5fe547dc0025d979ca1c
yoniffafan <yoniffafan@gmail.com>
2017-04-25 19:42:01 +0700

    add tabunganAct, mainAct, RegisterAct

---
+++ app/src/main/java/me/yoniffafan/android/celengin/tabunganActivity.java
@@ -0,0 +1,13 @@
+package me.yoniffafan.android.celengin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class tabunganActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tabungan);
+    }
+}
